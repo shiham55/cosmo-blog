5a5ed39268a010084744953a700dc25580a68eaf
Shiham M <shiham.mohamed@sg.ey.com>
2019-09-22 23:15:40 +0800

    initial comit

---
+++ ComoBlog.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ComoBlog.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
