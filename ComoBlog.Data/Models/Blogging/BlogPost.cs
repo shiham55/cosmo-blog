74a1269147fa5671a30f3f4cee1182b45329db13
Shiham M <shiham.mohamed@sg.ey.com>
2019-09-23 22:26:11 +0800

    bloggin model added

---
+++ ComoBlog.Data/Models/Blogging/BlogPost.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace ComoBlog.Data.Models.Blogging
+{
+    public class BlogPost
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "content")]
+        public string Content { get; set; }
+
+        [JsonProperty(PropertyName = "createdDate")]
+        public string CreatedDate { get; set; }
+
+        [JsonProperty(PropertyName = "publishedDate")]
+        public string PublishedDate { get; set; }
+
+        [JsonProperty(PropertyName = "isPublished")]
+        public bool IsPublished { get; set; }
+
+        [JsonProperty(PropertyName = "tags")]
+        public List<Tag> Tags { get; set; }
+    }
+}
---
+++ ComoBlog.Data/Models/Blogging/Tag.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComoBlog.Data.Models.Blogging
+{
+    public class Tag
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        [JsonProperty(PropertyName = "isActive")]
+        public bool IsActive { get; set; }
+    }
+}
