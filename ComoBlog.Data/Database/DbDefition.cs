4a8f878d1924582ad4a0c12d0876db538e12c94a
Shiham M <shiham.mohamed@sg.ey.com>
2019-10-03 11:25:24 +0800

    db migration skelton for no orm way

---
+++ ComoBlog.Data/Database/DbDefition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ComoBlog.Data.Database
+{
+    //https://andrewlock.net/running-async-tasks-on-app-startup-in-asp-net-core-3/
+
+    //https://docs.microsoft.com/en-us/dotnet/api/microsoft.azure.documents.client.documentclient.createdatabaseifnotexistsasync?view=azure-dotnet
+    public class DbDefition
+    {
+        private static string accountURI = "Your URI";
+        private static string accountKey = "Your Key";
+        public static string DatabaseId { get; private set; } = "ToDoList";
+        public static string ToDoItemsId { get; private set; } = "ToDoItems";
+
+       
+    }
+}
---
+++ ComoBlog.Data/Database/DbMigrationService.cs
@@ -0,0 +1,38 @@
+﻿using ComoBlog.Data.Models;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ComoBlog.Data.Database
+{
+    public class DbMigrationService : IHostedService
+    {
+        // We need to inject the IServiceProvider so we can create 
+        // the scoped service, MyDbContext
+        private readonly IServiceProvider _serviceProvider;
+        public DbMigrationService(IServiceProvider serviceProvider)
+        {
+            _serviceProvider = serviceProvider;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            // Create a new scope to retrieve scoped services
+            using (var scope = _serviceProvider.CreateScope())
+            {
+                // Get the DbContext instance
+                //var myDbContext = scope.ServiceProvider.GetRequiredService<BlogDbContext>();
+
+                //Do the migration asynchronously
+                //await myDbContext.Database.MigrateAsync();
+            }
+        }
+
+        // noop
+        public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+    }
+}
